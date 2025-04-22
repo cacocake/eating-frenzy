e37cdbaf945c4cf9685b74c63369ca66fc2f4ae9
cacocake <hugofqc92@gmail.com>
2025-04-22 17:26:59 +0100

    Make prop fall through hole in a more realistic manner
    - Fix bug where object would fall through the floor if the player moved away from the object

--- Assets/PlayerHole.cs
+++ Assets/PlayerHole.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class PlayerHole : MonoBehaviour {
     [SerializeField] private float speed = 5.0f;
 
-    void OnTriggerStay(Collider collider) {
-        if(!(collider.gameObject.tag == "Consumable")) {
-            return;
-        }
-
-        collider.gameObject.layer = LayerMask.NameToLayer("HoleLayer");
-    }
-
     void Update() {
         float moveSpeed = speed * Time.deltaTime;
         float horizontalInput = Input.GetAxis("Horizontal");
