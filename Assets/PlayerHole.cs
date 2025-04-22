7164bf8f41a415d603b2aa16c830faab5af056eb
cacocake <hugofqc92@gmail.com>
2025-04-22 13:05:21 +0100

    First Commit

---
+++ Assets/PlayerHole.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHole : MonoBehaviour {
+    [SerializeField] private float speed = 5.0f;
+
+    void Update() {
+        float moveSpeed = speed * Time.deltaTime;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(horizontalInput * moveSpeed, 0.0f, verticalInput * moveSpeed);
+    }
+}
