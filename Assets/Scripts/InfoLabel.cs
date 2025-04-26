1d15f796e787d62f7732f97af16623a1a612368f
cacocake <hugofqc92@gmail.com>
2025-04-26 16:44:53 +0100

    Improve UI
    - Change PointsLabel class to InfoLabel to be more generic
    - Add progress bars and a section for the timer instead of having simple labels on screen
    - Add current level label
    - Add particles to the level label when leveling up
    - Tweak MapPopulator values
    - Add Violet Theme UI assets

---
+++ Assets/Scripts/InfoLabel.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class InfoLabel : MonoBehaviour {
+    protected GameManager _manager;
+    protected string _labelTextFormat;
+
+    protected virtual void Start() {
+        _manager = GameManager.Instance;
+    }
+
+    public void UpdateLabel(params object[] values) {
+        if(_labelTextFormat == null) {
+            Debug.LogError("Label text prefix is not set.");
+            return;
+        }
+        string formattedText = string.Format(_labelTextFormat, values);
+        GetComponent<TextMeshProUGUI>().SetText(formattedText);
+    }
+}
