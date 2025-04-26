2d108a12681f1b7c7b580029096da4750f3baae7
cacocake <hugofqc92@gmail.com>
2025-04-26 12:07:45 +0100

    Add camera pan out upon leveling up

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,15 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    [SerializeField] private float _pullBackIncreaseFactor = 0.2f;
+    private CinemachineTransposer _transposer;
     private ConsumableObject _consumableObjectInBetweenPlayerAndCamera;
+    private Vector3 _baseOffset;
+    private Vector3 _targetOffset;
+
+    private void Awake() {
+        _transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
+        _baseOffset = _transposer.m_FollowOffset;
+        _targetOffset = _baseOffset;
+    }
+    void OnEnable() {
+        GameManager.OnLevelUp += TriggerAdaptToLevelUp;
+    }
+
+    void OnDisable() {
+        GameManager.OnLevelUp -= TriggerAdaptToLevelUp;
+    }
+
     void Update() {
         PlayerHole player = GameObject.FindWithTag("PlayerHoleCharacter").GetComponent<PlayerHole>();
         if (player == null) {
-            Debug.LogError("PlayerHole component not found on GameObject with tag 'Player'.");
             return;
+        }
+
+        if(_targetOffset != _transposer.m_FollowOffset) {
+            _transposer.m_FollowOffset = Vector3.Lerp(_transposer.m_FollowOffset, _targetOffset, Time.deltaTime);
         }
 
         Vector3 direction = player.transform.position - transform.position;
@@ -33,5 +53,10 @@ public class PlayerCamera : MonoBehaviour
                 }
             }
         }
+    }
+    private void TriggerAdaptToLevelUp() {
+        _targetOffset = _baseOffset + new Vector3(0.0f, 
+                                                  _pullBackIncreaseFactor * GameManager.Instance.CurrentLevel, 
+                                                  -_pullBackIncreaseFactor * GameManager.Instance.CurrentLevel);
     }
 }
